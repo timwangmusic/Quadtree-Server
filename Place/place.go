31d8ffe03ab049c359d4126dc37b728205456167
Tim Wang <weihewang2012@gmail.com>
2019-12-17 16:23:12 -0800

    add json fields to struct place

--- Place/place.go
+++ Place/place.go
@@ -1,13 +1,13 @@
 package place
 
 type GeoLocation struct {
-	Lat float64
-	Lng float64
+	Lat float64 `json:"lat"`
+	Lng float64 `json:"lng"`
 }
 
 type Place struct {
-	Id       string
-	Location GeoLocation
-	Name     string
-	Type     string
+	Id       string `json:"id"`
+	Location GeoLocation `json:"location"`
+	Name     string `json:"name"`
+	Type     string `json:"type"`
 }
